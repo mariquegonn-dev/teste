cb9f87b6402b258cbf72b5d06d78c2d89dd86af3
mariquegonn-dev <ocatodev@gmail.com>
2024-12-16 09:09:33 -0300

    feat: add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen">
       <iframe
-        src="https://my.spline.design/untitled-f1091278a5234ad411de6264ec5739da/"
+        src="https://my.spline.design/particles-f0eb7b18c76c4fc2c05d48630d0daf69/"
         width="100%"
         height="100%"
       ></iframe>
